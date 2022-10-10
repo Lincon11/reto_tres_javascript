47b8f45d2f09cd88b56721102701c6f9a3eb99b9
Lincon11 <eibraham37@gmail.com>
2022-10-10 16:39:23 -0500

    Programa que encuentra tu personaje favorito de La casa del dragon

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 var scanf = require('scanf')
 
+let LaCasaDelDragon = ["Rhaenerya", "Daemon", "Viserys", "Alicent"];
+let i = 0;
+
+console.log('Encuentra tu personaje favorito de House Of Dragon');
+console.log('\n');
+console.log('Escribe tu personaje favorito');
+console.log('\n');
+
+var personajeFavorito = scanf("%S").toLocaleLowerCase();;
+
+
+for (let personaje = 0; personaje < LaCasaDelDragon.length; personaje++) {
+    if (LaCasaDelDragon[personaje].toLocaleLowerCase() === personajeFavorito) {
+        console.log('Personaje encontardo')
+        console.log(`${personaje + 1}. ${LaCasaDelDragon[personaje]}`)
+    } else{
+        if(i < LaCasaDelDragon.length){
+            i++;
+            if(LaCasaDelDragon.length === i)
+            console.log('Personaje no encontrado');
+          }
+        }
+
+}
+
+
+
+
+
+
